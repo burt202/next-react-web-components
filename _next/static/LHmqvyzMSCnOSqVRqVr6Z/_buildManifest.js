2d64896236ccbc4ddf32293ce677fab288592d28
Aaron Burtnyk <aaron.burtnyk@britishgas.co.uk>
2023-02-24 15:00:55 +0000

    Updates

---
+++ _next/static/LHmqvyzMSCnOSqVRqVr6Z/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/bc1c4c33-0bdf4f161e184bcd.js","static/chunks/pages/index-21df2eb99b0a86eb.js"],"/_error":["static/chunks/pages/_error-942918685354aae9.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
